1f59fcce83c23c1d03550d29f1eb2df5dc479f87
bala-amypo <bala@amypo.in>
2025-12-17 04:25:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,10 +1,12 @@
 package com.example.demo.entity;
 
-import 
+import java.time.LocalDate;
+
 public class Student {
     private Long id;
     private String name;
     private String dept;
     private LocalDate dob;
     private float cgpa;
+    
 }
