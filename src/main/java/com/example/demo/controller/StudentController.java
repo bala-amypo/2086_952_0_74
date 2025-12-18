dfc10b983f13c3fe7ea8a23d680218b50298b9e4
bala-amypo <bala@amypo.in>
2025-12-18 04:47:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
-
+import.java.util.List;
+import.java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
@@ -16,9 +19,12 @@ public class StudentController {
     public Student addStudent(@RequestBody Student st){
         return stdser.poststudent(st);
     }
+    @GetMapping("/getall")
+    public List<Student> get(){
+        return stdser.getAllStudents();
+    }
     @GetMapping("/getById/{id}")
     public Optional<Student> getId(@PathVariable Long id){
         return stdser.getById(id);
-
     }
 }
