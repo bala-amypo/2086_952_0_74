89b001fba3a2cd132decbb2647cffe0b3b512ac0
bala-amypo <bala@amypo.in>
2025-12-18 09:45:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
 import jakarta.validation.Valid;
-import.jakarta.validation.constraints.*;
+import jakarta.validation.constraints.*;
+
 
 @RestController
 public class StudentController {
